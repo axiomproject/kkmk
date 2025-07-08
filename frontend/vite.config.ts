e7e87d4c0ff23a1d64f4dbb90768345989925932
axiomproject <johncharlesvergara01@gmail.com>
2025-07-09 07:16:47 +0800

    yes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,17 +3,27 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
         manualChunks: undefined,
-        assetFileNames: 'assets/[name].[hash].[ext]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.')
+          const extType = info[info.length - 1]
+          if (/\.(css)$/.test(assetInfo.name)) {
+            return `assets/css/[name].[hash][extname]`
+          }
+          if (/\.(png|jpe?g|gif|svg|ico|webp)$/.test(assetInfo.name)) {
+            return `assets/img/[name].[hash][extname]`
+          }
+          return `assets/[name].[hash][extname]`
+        },
+        chunkFileNames: 'assets/js/[name].[hash].js',
+        entryFileNames: 'assets/js/[name].[hash].js',
       }
     }
   },
