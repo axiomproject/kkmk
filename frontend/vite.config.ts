510d2e816b5330869b86cf93b4a5fd99e43ed62a
axiomproject <johncharlesvergara01@gmail.com>
2025-07-09 06:31:59 +0800

    tewst

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,22 +3,14 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
     sourcemap: false,
-    chunkSizeWarningLimit: 1600,
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
       output: {
-        manualChunks: undefined,
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
+        manualChunks: undefined
       }
     }
   },
