4969d17f96ba03e0c39dc6faa27efb8e8e9aa965
axiomproject <johncharlesvergara01@gmail.com>
2025-07-09 00:49:04 +0800

    test

---
+++ frontend/src/types/leaflet.d.ts
@@ -0,0 +1,33 @@
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import L from 'leaflet';
+
+declare module 'react-leaflet' {
+  interface MapContainerProps {
+    center?: L.LatLngExpression;
+    zoom?: number;
+    children?: React.ReactNode;
+    className?: string;
+    scrollWheelZoom?: boolean;
+    style?: React.CSSProperties;
+    attributionControl?: boolean;
+  }
+
+  interface TileLayerProps {
+    url: string;
+    attribution?: string;
+  }
+
+  interface MarkerProps {
+    position: L.LatLngExpression;
+    icon?: L.Icon;
+    children?: React.ReactNode;
+  }
+}
+
+declare module 'leaflet' {
+  export interface Layer {
+    _map?: L.Map;
+  }
+  export type LatLngTuple = [number, number];
+  export type LatLngExpression = LatLngTuple | { lat: number; lng: number };
+}
