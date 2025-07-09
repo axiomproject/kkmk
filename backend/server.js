d8297dcc4164c6ff2dae75ec61798989d042d0fa
axiomproject <johncharlesvergara01@gmail.com>
2025-07-09 08:05:34 +0800

    test

--- backend/server.js
+++ backend/server.js
@@ -43,10 +43,10 @@ db.connect()
     console.error('Error connecting to PostgreSQL:', error);
   });
 
-// Update CORS configuration to allow Netlify domain
+// Update CORS configuration
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://kmfi.netlify.app', 'https://kkmkpayatas.onrender.com', 'http://localhost:5173']
+    ? process.env.CORS_ORIGIN.split(',')
     : 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
